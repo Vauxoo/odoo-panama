f3bc13ec59c42e6f4896fde1b06acedef1ecfb64
luistorresm <luis_t@vauxoo.com>
2014-10-27 13:31:51 -0600

    [ADD][hr_employee_perception]Added module to set perception type of employee

---
+++ hr_employee_perception/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{"name": "HR employee perception type",
+    "version": "1.0",
+    "depends": ["hr",
+                ],
+    "author": "Vauxoo",
+    "description": """
+This module adds field to set perception type in employee
+    """,
+    "website": "http://www.vauxoo.com",
+    "category": "Localization/Panamá",
+    "demo": [],
+    "data": [
+        "view/hr_employee_view.xml"
+    ],
+    "test": [],
+    "images": [],
+    "auto_install": False,
+    "application": True,
+    "installable": True, }
---
+++ hr_employee_perception/model/hr_employee.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+'''
+This file adds field to set perception type in employee
+'''
+from openerp import models, fields, api
+
+
+class hr_employee(models.Model):
+    '''
+    Inherit hr.employee to added field perception type of the employee
+    '''
+    _inherit = 'hr.employee'
+    
+    @api.model
+    def _get_perception_types(self):
+        types = [
+            ('5_individual', '5- Individual'),
+            ('6_joint', '6- Joint'),
+            ]
+        return types
+
+    perception_type = fields.Selection(_get_perception_types,
+        'Perception Type', help='Perception type of the employee. Use: \n' \
+        '- 5 to declaration individual.\n- 6 to joint declaration, when the '\
+        'spouse not work.')
