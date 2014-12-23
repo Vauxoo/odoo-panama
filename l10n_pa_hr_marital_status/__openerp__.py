1e363d2d796f40414ac82c2da492dbc183b5cb78
sergioernestotostadosanchez <sergio@vauxoo.com>
2014-12-23 15:31:25 -0600

    [ADD][l10n_pa_hr_marital_status] A new module is added to extend another option in the marital field for employee's information

---
+++ l10n_pa_hr_marital_status/__openerp__.py
@@ -0,0 +1,48 @@
+# coding:utf8
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Sergio Ernesto Tostado Sánchez (sergio@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    "name": "HR Employee Marital Status Extension",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Localization/Panamá",
+    "description": """
+        This module makes an extension for the marital status options
+        for  the employees, adding  "Cohabitation" to  the previously
+        expressed.
+    """,
+    "website": "http://www.vauxoo.com",
+    "license": "",
+    "depends": [
+        "hr"
+    ],
+    "demo": [],
+    "data": [],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False
+}
