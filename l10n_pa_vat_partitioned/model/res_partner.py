87169d28da0b9fda41524ad3dcb85e1628f087f7
sergioernestotostadosanchez <sergio@vauxoo.com>
2015-01-06 01:55:50 -0600

    [IMP][l10n_pa_vat_partitioned] Translation, pylint and flake8 and code improvement for integrity in the VAT component fields were done

--- l10n_pa_vat_partitioned/model/res_partner.py
+++ l10n_pa_vat_partitioned/model/res_partner.py
@@ -23,65 +23,76 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from openerp import models, fields, api
+from openerp.tools.translate import _
 
 
 class ResPartner(models.Model):
 
+    """ This class is inherited to add three new fields to can edit
+        VAT field value """
+
     # Will be part of res.partner model
     _inherit = "res.partner"
 
-    @api.one
-    def _get_panama_default(self):
-        print "%s ------------------" % self.ref('base.pa')
-        return self.ref('base.pa')
-
+    # COMPUTES AND INVERSE
     @api.one
     @api.depends('vat')
     def _get_l10n_pa_ruc_country_id(self):
-        print "GET ACTIVATED ************************** COUNTRY"
+        """ Get the country object from the VAT field """
         if self.vat:
             country_code = None
             for country_id in self.env['res.country'].search(
                     [('code', '=', self.vat[:2])]):
                 country_code = country_id
             self.l10n_pa_ruc_country_id = country_code
-
-    @api.one
-    def _set_l10n_pa_ruc_country_id(self):
-        print "SET ACTIVATED ************************** COUNTRY"
-        if self.l10n_pa_ruc_country_id:
-            self.vat = "%s%s" % (self.l10n_pa_ruc_country_id.code, self.vat[2:])
+        else:
+            self.l10n_pa_ruc_country_id = self.env['res.country'].\
+                search([('code', '=', 'PA')])
 
     @api.one
     @api.depends('vat')
     def _get_l10n_pa_ruc(self):
-        print "GET ACTIVATED ************************** RUC"
+        """ Get the RUC value from the VAT field """
         if self.vat:
             self.l10n_pa_ruc = self.vat[2:len(self.vat)-4]
-
-    @api.one
-    def _set_l10n_pa_ruc(self):
-        print "SET ACTIVATED ************************** RUC"
-        if self.l10n_pa_ruc_country_id:
-            self.vat = "%s%s%s" % (self.vat[:2], self.l10n_pa_ruc, self.vat[:len(self.vat)-4])
+        else:
+            self.l10n_pa_ruc = "--"
 
     @api.one
     @api.depends('vat')
     def _get_l10n_pa_ruc_dv(self):
-        print "GET ACTIVATED ************************** DV"
+        """ Get the DV value from the VAT field """
         if self.vat:
             self.l10n_pa_ruc_dv = self.vat[-2:]
+        else:
+            self.l10n_pa_ruc_dv = "--"
 
     @api.one
-    def _set_l10n_pa_ruc_dv(self):
-        print "SET ACTIVATED ************************** DV"
+    @api.depends('l10n_pa_ruc_country_id', 'l10n_pa_ruc', 'l10n_pa_ruc_dv')
+    def _get_new_vat(self):
+        """ Get the value for VAT field through the 3 component fields """
+        new_vat = ""
+        if self.l10n_pa_ruc_country_id:
+            new_vat += self.l10n_pa_ruc_country_id.code
+        if self.l10n_pa_ruc:
+            new_vat += self.l10n_pa_ruc
         if self.l10n_pa_ruc_dv:
-            self.vat = "%s%s" % (self.vat[:-2], self.l10n_pa_ruc_dv)
+            new_vat += "DV%s" % self.l10n_pa_ruc_dv
+        self.vat = new_vat
 
-    # COLUMNS
+    # COLUMNS (New fields)
     l10n_pa_ruc_country_id = fields.Many2one(
-        'res.country', ondelete="set null", default=_get_panama_default, compute='_get_l10n_pa_ruc_country_id', inverse='_set_l10n_pa_ruc_country_id')
+        'res.country', ondelete="set null",
+        compute='_get_l10n_pa_ruc_country_id',
+        inverse='_get_new_vat',
+        help=_("Country to form the TIN field"))
     l10n_pa_ruc = fields.Char(
-        string="RUC", size=25, compute='_get_l10n_pa_ruc', inverse='_set_l10n_pa_ruc')
+        string="RUC", size=25,
+        compute='_get_l10n_pa_ruc',
+        inverse='_get_new_vat',
+        help=_("RUC value to form the TIN field"))
     l10n_pa_ruc_dv = fields.Char(
-        string="DV", size=2, compute='_get_l10n_pa_ruc_dv', inverse='_set_l10n_pa_ruc_dv')
+        string="DV", size=2,
+        compute='_get_l10n_pa_ruc_dv',
+        inverse='_get_new_vat',
+        help=_("DV value to form the TIN field"))
