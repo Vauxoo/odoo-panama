820dfacc66a5b31e2888d17cacb26a567c6c214f
Hugo Adan <hugo@vauxoo.com>
2015-10-16 16:49:24 +0000

    [IMP]added translation files

--- l10n_pa_localization/__openerp__.py
+++ l10n_pa_localization/__openerp__.py
@@ -25,9 +25,9 @@
         "data/res_country_states_district_township.xml",
         "data/res_country_states_district_township_hood.xml",
         "view/res_city_view.xml",
+        "security/localization_security.xml",
+        "security/ir.model.access.csv"
         # "view/partner_address_view.xml",
-        # "security/city_security.xml",
-        # "security/ir.model.access.csv"
     ],
     "installable": True,
 }
