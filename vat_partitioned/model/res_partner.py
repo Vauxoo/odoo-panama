2b42fadf135cf51908f48d2d2d5a7453ab88f8c2
luistorresm <luis_t@vauxoo.com>
2016-01-21 20:26:28 +0000

    [FIX][vat_partitioned]Use commercial_partner_id to get data of VAT

--- vat_partitioned/model/res_partner.py
+++ vat_partitioned/model/res_partner.py
@@ -25,7 +25,6 @@
 import re
 
 from openerp import models, fields, api
-from openerp.tools.translate import _
 
 
 class ResPartner(models.Model):
@@ -41,62 +40,60 @@ class ResPartner(models.Model):
     _check_country_vat_dv_re = re.compile(_regex_country_vat_dv)
 
     # COMPUTES AND INVERSE
-    @api.one
     @api.depends('vat')
     def _get_vat_country_id(self):
         """ Get the country object from the VAT field """
-        self.vat_country_id = None
-        match_country_vat_dv = self._check_country_vat_dv_re.match(
-            self.vat or '')
-        if match_country_vat_dv:
-            country_code = match_country_vat_dv.group('country')
-            country_ids = self.env['res.country'].search(
-                [('code', '=', country_code)],
-                limit=1)
-            country_id = country_ids[0].id if country_ids else None
-            if country_id:
-                self.vat_country_id = country_id
-        elif not self.id:
-            # This section process "default" case.
-            company_pool = self.env['res.company']
-            company_default_id = company_pool._company_default_get(
-                'res.partner')
-            self.vat_country_id = company_pool.browse(
-                company_default_id).country_id.id
+        for partner in self:
+            partner.vat_country_id = None
+            match_country_vat_dv = partner.commercial_partner_id.\
+                _check_country_vat_dv_re.match(
+                    partner.commercial_partner_id.vat or '')
+            if match_country_vat_dv:
+                country_code = match_country_vat_dv.group('country')
+                country_ids = self.env['res.country'].search(
+                    [('code', '=', country_code)], limit=1)
+                if country_ids:
+                    partner.vat_country_id = country_ids[0].id
+            elif not partner.id:
+                # This section process "default" case.
+                company_pool = self.env['res.company']
+                company_default_id = company_pool._company_default_get(
+                    'res.partner')
+                partner.vat_country_id = company_pool.browse(
+                    company_default_id).country_id.id
 
-    @api.one
     @api.depends('vat')
     def _get_vat_alone(self):
         """ Get the RUC value from the VAT field """
-        self.vat_alone = ""
-        match_country_vat_dv = self._check_country_vat_dv_re.match(
-            self.vat or '')
-        if match_country_vat_dv:
-            self.vat_alone = match_country_vat_dv.group('vatwdv') \
-                or match_country_vat_dv.group('vat')
+        for partner in self:
+            partner.vat_alone = ""
+            match_country_vat_dv = partner.commercial_partner_id.\
+                _check_country_vat_dv_re.match(
+                    partner.commercial_partner_id.vat or '')
+            if match_country_vat_dv:
+                partner.vat_alone = match_country_vat_dv.group('vatwdv') \
+                    or match_country_vat_dv.group('vat')
 
-    @api.one
     @api.depends('vat')
     def _get_vat_dv(self):
         """ Get the DV value from the VAT field """
-        self.vat_dv = ""
-        match_country_vat_dv = self._check_country_vat_dv_re.match(
-            self.vat or '')
-        if match_country_vat_dv:
-            self.vat_dv = match_country_vat_dv.group('dv')
+        for partner in self:
+            partner.vat_dv = ""
+            match_country_vat_dv = partner.commercial_partner_id.\
+                _check_country_vat_dv_re.match(
+                    partner.commercial_partner_id.vat or '')
+            if match_country_vat_dv:
+                partner.vat_dv = match_country_vat_dv.group('dv')
 
-    @api.one
     @api.depends('vat_country_id', 'vat_alone', 'vat_dv')
     def _get_new_vat(self):
         """ Get the value for VAT field through the 3 component fields """
-        new_vat = ""
-        if self.vat_country_id:
-            new_vat += self.vat_country_id.code
-        if self.vat_alone:
-            new_vat += self.vat_alone
-        if self.vat_dv:
-            new_vat += "DV%s" % self.vat_dv
-        self.vat = new_vat
+        for partner in self:
+            new_vat = partner.commercial_partner_id.vat_country_id.code or ''
+            new_vat += partner.commercial_partner_id.vat_alone or ''
+            if partner.vat_dv:
+                new_vat += "DV%s" % partner.commercial_partner_id.vat_dv
+            partner.vat = new_vat
 
     # COLUMNS (New fields)
     vat_country_id = fields.Many2one(
@@ -104,14 +101,14 @@ class ResPartner(models.Model):
         ondelete="set null",
         compute='_get_vat_country_id',
         inverse='_get_new_vat',
-        help=_("Country to form the TIN field"))
+        help="Country to form the TIN field")
     vat_alone = fields.Char(
         string="RUC", size=25,
         compute='_get_vat_alone',
         inverse='_get_new_vat',
-        help=_("RUC value to form the TIN field"))
+        help="RUC value to form the TIN field")
     vat_dv = fields.Char(
         string="DV", size=2,
         compute='_get_vat_dv',
         inverse='_get_new_vat',
-        help=_("DV value to form the TIN field"))
+        help="DV value to form the TIN field")
